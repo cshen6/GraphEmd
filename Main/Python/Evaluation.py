b8479c55ba5690b3b7d550a17471c4faa3550721
Ariel Lubonja <ariellubonja@live.com>
2022-04-02 16:22:34 -0400

    Working evaluation code simple test case

--- Main/Python/Evaluation.py
+++ Main/Python/Evaluation.py
@@ -50,3 +50,35 @@ class Evaluation:
         ari = adjusted_rand_score(Y_result, Y_original.reshape(-1,))
 
         return ari
+
+
+# Code to test functions
+class Encoder_case:
+    def __init__(self, A,Y,n):
+        Encoder_case.X = A
+        Encoder_case.Y = Y
+        Encoder_case.n = n
+
+
+if __name__ == '__main__':
+
+    import numpy as np
+
+    A = np.ones((5,5))
+    A[0,4] = 0
+    A[4,0] = 0
+    np.fill_diagonal(A, 0)
+
+    Y = np.array([[0,0,0,1,1]]).reshape((5,1))
+
+    print(A)
+    print(Y)
+
+    Encoder_case5 = Encoder_case(A,Y,5)
+
+    from Main.Python.DataPreprocess import DataPreprocess
+
+    Dataset = DataPreprocess(Encoder_case5, Laplacian = False, DiagA = False)
+    print(Dataset.X)
+    print(Dataset.Y)
+    print(Dataset.n)
